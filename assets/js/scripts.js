463b32d92185e1f712fa93a61ec6435e81636662
Andrey Novikov <bodrovshum@gmail.com>
2021-04-26 17:02:08 +0300

    Find metro

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -215,6 +215,38 @@ function init() {
       iconImageOffset: [-23, -23]
     });
     myMap.geoObjects.add(appartmentPlacemark);
+
+
+      // Поиск станций метро.
+      ymaps.geocode(myMap.getCenter(), {
+          /**
+           * Опции запроса
+           * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/geocode.xml
+           */
+          // Ищем только станции метро.
+          kind: 'metro',
+          // Запрашиваем не более 20 результатов.
+          results: 3
+      }).then(function (res) {
+
+          //console.log(res.geoObjects)
+          // Задаем изображение для иконок меток.
+          res.geoObjects.options.set('preset', 'islands#redCircleIcon');
+          res.geoObjects.events
+          // При наведении на метку показываем хинт с названием станции метро.
+              .add('mouseenter', function (event) {
+                  var geoObject = event.get('target');
+                  myMap.hint.open(geoObject.geometry.getCoordinates(), geoObject.getPremise());
+              })
+              // Скрываем хинт при выходе курсора за пределы метки.
+              .add('mouseleave', function (event) {
+                  myMap.hint.close(true);
+              });
+          // Добавляем коллекцию найденных геообъектов на карту.
+          myMap.geoObjects.add(res.geoObjects);
+          // Масштабируем карту на область видимости коллекции.
+          myMap.setBounds(res.geoObjects.getBounds());
+      });
   });
 }
 
